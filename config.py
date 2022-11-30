cc2db0d1627d0e612325655fa49545bb0623dea2
lizhihao6 <lizhihao6@outlook.com>
2022-11-30 15:20:46 +0800

    fixed bug

--- config.py
+++ config.py
@@ -6,16 +6,18 @@ from __future__ import unicode_literals
 
 # Authentication for user filing issue (must have read/write access to repository to add issue to)
 USERNAME = 'lizhihao6'
-TOKEN = 'changeme'
+TOKEN = ''
 
 # The repository to add this issue to
 REPO_OWNER = 'lizhihao6'
-REPO_NAME = 'lizhihao6'
+REPO_NAME = 'get-daily-arxiv-noti'
 
 # Set new submission url of subject
-NEW_SUB_URL = 'https://arxiv.org/list/cs/new'
+NEW_SUB_URL = 'https://arxiv.org/list/cs.CV/new'
 
 # Keywords to search
-KEYWORD_LIST = ["event camera", "white balance", "isp", "compression", 
-                "image signal processing", "image signal process",
-                "raw", "raw image"]
+KEYWORD_LIST = ["events", "event camera", "events camera", 
+                "white balance", "color contrast", "AWB",
+                "ISP", "image signal processing", "image signal process",
+                "compression",
+                "RAW", "raw image"]
